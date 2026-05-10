fc09686e47115213a95416ed48c83f7e572a9fa2
Küzmös Áron <kuzmos.aron@gmail.com>
2026-05-10 13:22:31 +0200

    Initial commit and MouseController implemented

---
+++ Assets/Scripts/Player/MouseController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    public float MouseSensitivity = 500f;
+    float xRotation = 0f;
+    float yRotattion = 0f;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * Time.deltaTime * MouseSensitivity;
+        float mouseY = Input.GetAxis("Mouse Y") * Time.deltaTime * MouseSensitivity;
+
+        xRotation -= mouseY;
+        yRotattion += mouseX;
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        transform.localRotation = Quaternion.Euler(xRotation,yRotattion,0f);
+    }
+}
