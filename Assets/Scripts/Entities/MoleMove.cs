f3e04049e2610dc4d1afb369d77a8e72856d3904
Küzmös Áron <kuzmos.aron@gmail.com>
2026-07-30 13:18:57 +0200

    Added burrow

---
+++ Assets/Scripts/Entities/MoleMove.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+
+public class MoleMove : EntityMove
+{
+    [SerializeField] float BurrowSpeed;
+    [SerializeField] float BurrowCoolDown;
+    [SerializeField] float BurrowDuration;
+    float currentBurrowCoolDown;
+    float currentBurrowDuration;
+    burrowEffect burrowEffect;
+
+    protected override void Start()
+    {
+        base.Start();
+        currentBurrowCoolDown = 0;
+        currentBurrowDuration = BurrowDuration;
+        burrowEffect = GetComponent<burrowEffect>();
+    }
+
+    protected override void Update()
+    {
+
+        if (isDead) return;
+        
+        PlayWalkingSound();
+        WalkingTimer -= Time.deltaTime;
+        
+        
+        
+
+        Collider[] colliders = Physics.OverlapSphere(transform.position, Range, Ally);
+        AllyNearby = colliders.Length > 0;
+
+        if (AllyNearby)
+        {
+            foreach (var collider in colliders)
+            {
+                if (Vector3.Distance(transform.position, collider.transform.position)
+                    <= MinDst)
+                {
+                    Target = collider.GetComponent<LivingAbstractClass>();
+                    MinDst = Vector3.Distance(transform.position,
+                        collider.transform.position);
+                }
+            }
+        }
+        Debug.Log($"cooldoen: {currentBurrowCoolDown}, duration: {currentBurrowDuration}");
+        if (burrowEffect.IsBurrowing)
+        {
+            currentBurrowDuration -= Time.deltaTime;
+        }
+        else
+        {
+            currentBurrowCoolDown -= Time.deltaTime;
+        }
+
+        if(currentBurrowDuration <= 0f && burrowEffect.IsBurrowing)
+        {
+            
+            burrowEffect.ToggleBurrow();
+            currentBurrowDuration = BurrowDuration;
+        }
+
+
+
+        if (Target != null)
+        {
+
+            bool validTarget = Target.GetComponent<LivingAbstractClass>() != null
+                || Target.GetComponent<Tower>().IsBuilt;
+            float dst = Vector3.Distance(transform.position, Target.transform.position);
+
+
+
+            if (!validTarget || dst > Range)
+            {
+                MinDst = float.MaxValue;
+                Target = null;
+                animator.SetBool("Walk", true);
+                IsWalking = true;
+                MoveTowardsWayPoints();
+                //Debug.Log($"Moving to waypoints");
+            }
+            else if (dst <= attackDst)
+            {
+                Turn(Target.transform.position);
+
+                animator.SetBool("Walk", false);
+                IsWalking = false;
+                EntitiesEvent.EntityAttack(gameObject);
+                //Debug.Log("Attacking target");
+            }
+            else
+            {
+               
+                if(currentBurrowCoolDown <= 0)
+                {
+                    
+                    burrowEffect.ToggleBurrow();
+                    animator.SetTrigger("Burrow");
+                    currentBurrowCoolDown = BurrowCoolDown;
+                }
+                else if(!burrowEffect.IsBurrowing)
+                {
+                    
+                    animator.SetBool("Walk", true);
+                    IsWalking = true;
+                    Turn(Target.transform.position);
+                }
+                
+                Vector3 newPos = Target.transform.position;
+                newPos.y = transform.position.y;
+                transform.position = Vector3.MoveTowards(transform.position,
+                newPos, speed * Time.deltaTime);
+                //Debug.Log($"Walking towards target {dst}");
+
+            }
+
+        }
+        else
+        {
+            IsWalking = true;
+            animator.SetBool("Walk", true);
+            MoveTowardsWayPoints();
+            //Debug.Log($"walking towards waypoints");
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/Entities/burrowEffect.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using UnityEngine;
+
+public class burrowEffect : MonoBehaviour
+{
+    [SerializeField] Transform originalPos;
+    [SerializeField] float BurrowDepth;
+    
+    [SerializeField] private ParticleSystem dirtParticles;
+    [SerializeField] float transitionDuration;
+    public bool IsBurrowing { get; private set; }
+
+    private void Start()
+    {
+        IsBurrowing = false;
+        originalPos = transform;
+        
+    }
+
+    public void ToggleBurrow()
+    {
+        if (!IsBurrowing)
+        {
+            StartCoroutine(Burrow());
+            
+        }
+        else
+        {
+            StartCoroutine(UnBurrow());
+        }
+    }
+
+
+    IEnumerator Burrow()
+    {
+        IsBurrowing = true;
+        Vector3 startPos = originalPos.localPosition;
+        Vector3 targetPos = new Vector3(startPos.x, BurrowDepth, startPos.z);
+        float time = 0;
+        //dirtParticles.Play();
+        while (time <= transitionDuration)
+        {
+
+            originalPos.localPosition = Vector3.Lerp(startPos, targetPos, time/transitionDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        originalPos.localPosition = targetPos;
+
+    }
+
+
+
+    IEnumerator UnBurrow()
+    {
+        IsBurrowing = false;
+        Vector3 startPos = originalPos.localPosition;
+        Vector3 targetPos = new Vector3(startPos.x, 0, startPos.z);
+        float time = 0;
+        //dirtParticles.Play();
+        while (time <= transitionDuration)
+        {
+
+            originalPos.localPosition = Vector3.Lerp(startPos, targetPos, time / transitionDuration);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        originalPos.localPosition = targetPos;
+    }
+}
